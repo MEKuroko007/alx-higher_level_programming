1ec8f697931481cd58dddee3b00068c2dc8a8134
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-09-10 19:43:45 +0100

    fix some scripts

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -29,35 +29,35 @@ listint_t *reverse_listint(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-    if (head == NULL || *head == NULL)
-        return (1);
+	if (head == NULL || *head == NULL)
+		return (1);
 
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
-    listint_t *temp = NULL;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *temp = NULL;
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
 
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
-    }
+		temp = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = temp;
+	}
 
-    if (fast != NULL)
-        slow = slow->next;
+	if (fast != NULL)
+		slow = slow->next;
 
-    while (prev != NULL && slow != NULL)
-    {
-        if (prev->n != slow->n)
-            return (0);
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
 
-        prev = prev->next;
-        slow = slow->next;
-    }
+		prev = prev->next;
+		slow = slow->next;
+	}
 
-    return (1);
+	return (1);
 }
