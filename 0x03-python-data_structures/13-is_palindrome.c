2375f578600a9afeb7e75f8439dd8df767f84ee2
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-09-10 19:18:15 +0100

    add some tasks

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a linked list in place.
+ * @head: Pointer to a pointer pointing to the first item in the list.
+ * Return: The new head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *current = *head, *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return *head;
+}
+
+/**
+ * is_palindrome - Checks if a linked list is a palindrome.
+ * @head: Pointer to a pointer pointing to the first item in the list.
+ * Return: 1 if the list is a palindrome, 0 otherwise.
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *temp = NULL;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+
+		temp = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = temp;
+	}
+
+
+	if (fast != NULL)
+		slow = slow->next;
+
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+
+		prev = prev->next;
+		slow = slow->next;
+	}
+
+	return (1);
+}
