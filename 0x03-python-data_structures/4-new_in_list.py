bf498ed8b114d2ab66ac264d3c108a17894f9065
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-09-10 19:35:17 +0100

    fix some scripts

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    tmp=my_list[:]
-    if idx >= 0 and idx < len(my_list):
-        tmp[idx] = element
-        return tmp
-    return my_list
+    n = len(my_list)
+    new = my_list.copy()
+    if idx < 0:
+        return new
+    elif idx > n - 1:
+        return new
+    else:
+        new[idx] = element
+        return new
