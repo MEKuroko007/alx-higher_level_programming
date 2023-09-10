23780c77474e9596bf373cb6609026c4692ed02b
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-09-10 10:02:18 +0100

    fix some scripts

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    char = sentence[0] if length > 0 else "None"
-    tup = length, char
+    first_character = sentence[0] if length > 0 else "None"
+    tup = length, first_character
     return(tup)
