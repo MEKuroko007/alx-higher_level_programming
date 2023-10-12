3c9cb09a918171232edb1937251ab7df049aa511
MEKuroko007 <essalhimu@gmail.com>
2023-10-12 12:05:04 +0200

    rectangle class with defining square class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+    class Base
+"""
 
 class Base:
+    """
+        This class will manage the id attribute for all the classes.
+        Arguments:
+            @id: The id for a specific instance.
+    """
+    
     __nb_objects = 0
     def __init__(self, id=None):
         if id is not None:
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    class square
+"""
+from models.rectangle import Rectangle
+class Square(Rectangle):
+    """
+        class square inherit from Rectangle class
+    """
+    pass
