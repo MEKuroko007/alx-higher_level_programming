ed84f4ca398d4e7e1b3b6f266ff5890203beb51f
MEKuroko007 <essalhimu@gmail.com>
2023-10-12 12:18:30 +0200

    square class width x width

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,31 @@
 from models.rectangle import Rectangle
 class Square(Rectangle):
     """
-        class square inherit from Rectangle class
+        Class Square inherits from Rectangle
     """
-    pass
+    def __init__(self, size, x=0, y=0, id=None):
+        
+        """Initialization a Square
+        """
+        # super().__init__(width, heigth , x, y, id)
+        super().__init__(size, size , x, y, id)
+    @property
+    def size(self):
+        """module Square size getter
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """module Square size setter
+        """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """module string represation of square
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
