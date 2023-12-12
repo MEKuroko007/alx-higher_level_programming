eb7a2f995f8d31a4b5adfa9a220c41d5419b2bc9
MEKuroko007 <essalhimu@gmail.com>
2023-12-12 10:00:29 +0100

    es6 javascript

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if ((width > 0) && (height > 0)) {
+      this.width = w;
+      this.height = height;
+    }
+  }
+}
+
+module.exports = Rectangle;
