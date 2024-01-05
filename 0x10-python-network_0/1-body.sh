6fb6d5a0e93c0b3b463f7ddad32bf8dc6364013b
MEKuroko007 <essalhimu@gmail.com>
2024-01-05 10:57:21 +0100

    create some scripts with curl

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends GET request and displays body of response
+curl -sL "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# DELETE request to URL passed as the first argument
+curl -sX "DELETE" "$1"
