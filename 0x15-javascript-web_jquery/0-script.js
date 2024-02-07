ac18c55249aae113219ba2b00937c715efe90a0c
MEKuroko007 <essalhimu@gmail.com>
2024-02-07 10:21:31 +0100

    update somethings

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// writing code to update colors
+
+const colorH = document.querySelector('header');
+colorH.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,3 @@
+// Writing js code to update color with css 
+
+$('header').css({ color: '#FF0000' });
